542e9e1363984fb68cb8e3f55af21935a6965913
maya <mayazaha2@gmail.com>
2021-03-09 23:15:08 +0200

    Closes #10

--- pet-clinic-data/src/main/java/guru/springframework/petclinicdata/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinicdata/services/PetService.java
@@ -2,12 +2,5 @@ package guru.springframework.petclinicdata.services;
 
 import guru.springframework.petclinicdata.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
