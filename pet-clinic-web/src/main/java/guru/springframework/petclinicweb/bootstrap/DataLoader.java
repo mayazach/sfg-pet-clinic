cc1cd13ddc4438f7dce63e8eb83c38573ebbd4ca
maya <mayazaha2@gmail.com>
2021-04-04 15:49:55 +0300

    Closes #44

--- pet-clinic-web/src/main/java/guru/springframework/petclinicweb/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinicweb/bootstrap/DataLoader.java
@@ -28,7 +28,7 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         int count = petTypeService.findAll().size();
-        if(count > 0) {
+        if(count == 0) {
             LoadData();
         }
     }
