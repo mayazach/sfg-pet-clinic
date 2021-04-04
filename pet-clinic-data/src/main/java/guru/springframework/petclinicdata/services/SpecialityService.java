8c51fc95afff841d325fcbf380a6f630ac27a384
maya <mayazaha2@gmail.com>
2021-04-04 14:31:56 +0300

    Closes #30

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinicdata/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package guru.springframework.petclinicdata.services;
+
+import guru.springframework.petclinicdata.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+}
